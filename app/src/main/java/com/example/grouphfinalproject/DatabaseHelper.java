b5d44268c148a3f49df73c143cc32f6ab586e6b5
Parth Dalwadi <pbdalwadi@gmail.com>
2020-02-15 19:55:29 -0500

    Merge branch 'master' of /Users/parthdalwadi/Desktop/Group-H-Android-Project with conflicts.

---
+++ app/src/main/java/com/example/grouphfinalproject/DatabaseHelper.java
@@ -0,0 +1,113 @@
+package com.example.grouphfinalproject;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+
+    public static final String DATABASE_NAME = "AllNotesData" ;
+    public static final int DATABASE_VERSION = 1;
+    public static final String TABLE_NAME = "MyNotes";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_TITLE = "Title";
+    public static final String COLUMN_DESCRIPTION = "Description";
+    public static final String COLUMN_LAT = "Latitude";
+    public static final String COLUMN_LNG = "Longitude";
+    public static final String COLUMN_CATEGORY = "Category";
+    public static final String COLUMN_TIMESTAMP = "Created Date-Time";
+
+
+
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME , null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        String sql = "CREATE TABLE " + TABLE_NAME + "(" +
+                COLUMN_ID + " INTEGER NOT NULL CONSTRAINT PK_Notes PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_TITLE + " varchar(200) NOT NULL, " +
+                COLUMN_DESCRIPTION + " varchar(200) NOT NULL, " +
+                COLUMN_CATEGORY + " varchar(200) NOT NULL, " +
+                COLUMN_TIMESTAMP + " varchar(200) NOT NULL, " +
+                COLUMN_LAT + " double NOT NULL, " +
+                COLUMN_LNG + " double NOT NULL);";
+
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        String sql = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+        db.execSQL(sql);
+        onCreate(db);
+    }
+
+
+    boolean addNote(String title, String description, String category, String CreatedTimeStamp, double lat, double lng){
+
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_DESCRIPTION, description);
+        cv.put(COLUMN_CATEGORY, description);
+        cv.put(COLUMN_TIMESTAMP, description);
+        cv.put(COLUMN_LAT, lat);
+        cv.put(COLUMN_LNG, lng);
+
+        return sqLiteDatabase.insert(TABLE_NAME, null, cv) != -1;
+    }
+
+    Cursor getAllPlaces(){
+
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        return sqLiteDatabase.rawQuery("SELECT * from " + TABLE_NAME, null);
+    }
+
+
+    boolean updateNote(int id, String title, String description, String category){
+
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_TITLE, title);
+        cv.put(COLUMN_DESCRIPTION, description);
+        cv.put(COLUMN_CATEGORY, description);
+
+
+        return sqLiteDatabase.update(TABLE_NAME, cv, COLUMN_ID + "=?", new String[]{String.valueOf(id)}) > 0 ;
+
+
+    }
+
+    /*
+    int numberOfResults(double lat, double lng){
+
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+        return sqLiteDatabase.rawQuery("SELECT * from " + TABLE_NAME + " WHERE " +
+                COLUMN_LAT+ " =?" + " AND " + COLUMN_LNG + " =? ", new String[]{String.valueOf(lat),String.valueOf(lng)})
+                .getCount();
+
+
+
+    }*/
+
+    boolean removeNote(String ColumnName, String Value){
+
+        SQLiteDatabase sqLiteDatabase = getWritableDatabase();
+
+        return sqLiteDatabase.delete(TABLE_NAME, ColumnName + "=?", new String[]{Value}) > 0 ;
+
+    }
+}
--- app/src/main/java/com/example/grouphfinalproject/NotesActivity.java
+++ app/src/main/java/com/example/grouphfinalproject/NotesActivity.java
@@ -5,13 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class NotesActivity extends AppCompatActivity {
 
+    private static final String NOTE_ID = "selectedNoteId" ;
     ImageButton imageButton;
     ListView listView;
+    ArrayList<Note> Notes;
+
+    private ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +28,27 @@ public class NotesActivity extends AppCompatActivity {
 
         imageButton = findViewById(R.id.addNotes);
         listView = findViewById(R.id.notesList);
+        Notes = new ArrayList<>();
 
 
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //make third activity
+                Intent i = new Intent(NotesActivity.this, NoteDetails.class);
+                startActivity(i);
+            }
+        });
 
-                System.out.println("Working");
 
-                Intent intent = new Intent(NotesActivity.this , MapsActivity.class);
-                startActivity(intent);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                Intent i = new Intent(NotesActivity.this, NoteDetails.class);
+                i.putExtra(NOTE_ID,Notes.get(position).getId());
+                startActivity(i);
 
             }
         });
@@ -39,5 +56,28 @@ public class NotesActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        refreshList();
+
+
+    }
+
+    public void refreshList(){
+
+        // this part has to be replaced by custom adaptor
+        String[] titleArray = new String[Notes.size()];
+
+        for(int i = 0; i < Notes.size(); i++){
+
+            titleArray[i] = Notes.get(i).getTitle();
+        }
+
+
+        arrayAdapter = new ArrayAdapter(this , android.R.layout.simple_list_item_1 ,titleArray );
+        listView.setAdapter(arrayAdapter);
+
+    }
 }
